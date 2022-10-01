4c78e50cc324497dc0421098c0f68bf3c7ab5f64
Jonathan <jkocevar@uoguelph.ca>
2022-10-01 11:14:27 +0000

    Added Main method

---
+++ src/main/java/tictactoe/tictactoe.java
@@ -0,0 +1,11 @@
+package tictactoe;
+
+public class tictactoe {
+  public static void main(String[] args) {
+      System.out.println("Welcome to 3x3 TicTacToe");
+      
+      gridBoard gameBoard = new gridBoard();
+      gameBoard.testgame();
+      
+  }
+}
